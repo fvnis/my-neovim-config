3221f07b7210d44a040b1ba869ff0fe309f906fd
Eduardo Fanis <eduardo.fanis@hotmail.com>
2024-03-27 01:13:35 -0300

    remove eslint

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,7 +8,6 @@ return {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports_reviser,
-        null_ls.builtins.diagnostics.eslit_d,
       }
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
